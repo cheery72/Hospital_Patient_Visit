f6ed2556360f3b68e7ac7eb6b0159864a0a84cfe
김경민 <nutmy52@naver.com>
2023-05-22 23:55:54 +0900

    refactor: Lombok, Validation 추가 및 적용
    - CreatePatientRequest Validation 추가

--- src/main/java/com/hdjunction/task/domain/Hospital.java
+++ src/main/java/com/hdjunction/task/domain/Hospital.java
@@ -1,10 +1,16 @@
 package com.hdjunction.task.domain;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Hospital {
 
     @Id
@@ -26,43 +32,6 @@ public class Hospital {
 
     @OneToMany(mappedBy = "hospital")
     private List<Visit> visits = new ArrayList<>();
-
-    public Hospital(Long id, String name, String sanatoriumNumber, String director, List<Patient> patients, List<Visit> visits) {
-        this.id = id;
-        this.name = name;
-        this.sanatoriumNumber = sanatoriumNumber;
-        this.director = director;
-        this.patients = patients;
-        this.visits = visits;
-    }
-
-    protected Hospital() {
-
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getSanatoriumNumber() {
-        return sanatoriumNumber;
-    }
-
-    public String getDirector() {
-        return director;
-    }
-
-    public List<Patient> getPatients() {
-        return patients;
-    }
-
-    public List<Visit> getVisits() {
-        return visits;
-    }
 
     public static Hospital of(){
         return new Hospital();
