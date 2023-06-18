7cc89ab2287647cace4fae75688067103a5cb5ae
김경민 <nutmy52@naver.com>
2023-06-18 23:20:28 +0900

    refactor: 환자 등록 번호 생성 리팩토링
    - 환자 등록 번호 생성 중복 시 에외처리

--- src/main/java/com/hdjunction/task/exception/ErrorCode.java
+++ src/main/java/com/hdjunction/task/exception/ErrorCode.java
@@ -6,8 +6,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ErrorCode {
     NOT_FOUND_HOSPITAL(-1000,"존재하지 않는 병원 입니다.",HttpStatus.NOT_FOUND),
-    NOT_FOUND_PATIENT(-1001,"존재하지 않는 환자 입니다.",HttpStatus.NOT_FOUND);
-
+    NOT_FOUND_PATIENT(-1001,"존재하지 않는 환자 입니다.",HttpStatus.NOT_FOUND),
+    FAILED_CREATE_PROCESS(-1002, "환자 생성이 실패했습니다. 잠시 후 다시 시도해주세요.",HttpStatus.INTERNAL_SERVER_ERROR);
 
     ErrorCode(Integer code, String message, HttpStatus httpStatus) {
         this.code = code;
